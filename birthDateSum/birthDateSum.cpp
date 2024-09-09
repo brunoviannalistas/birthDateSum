e284e7accdceec867bf0ab3de751290bf91ec0f2
bruno vianna <brunovianna.listas.0@gmail.com>
2024-09-09 12:50:13 +0200

    empty file

--- birthDateSum/birthDateSum.cpp
+++ birthDateSum/birthDateSum.cpp
@@ -1,19 +1,16 @@
-// birthDateSum.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// birthDateSum.cpp : This is tha main file
 //
+/*
 
-#include <iostream>
+Ask the user for the day, month and year of her birth. The year must have four digits. Add the numbers in the following fashion:
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+DD+MM+YYYY
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+For example, if the birth date is 01/07/2003, the result must be:
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+01+07+2003 = 2011
+
+Display the result of the operation
+*/
+
+// the code goes below this line
